3eddb1a98a4c1c458eae46aa57e2f6c522801659
lucas12f <37491@raphaeldisanto.com.br>
2026-09-02 19:23:22 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // Se o projeto for público, troque a senha do usuário do MongoDB e use
 // MONGODB_URI nas Environment Variables da Vercel.
 const MONGODB_URI =
-  "mongodb+srv://admin:admin@cluster0.z4p0psn.mongodb.net/?appName=Cluster0";
+  "mongodb+srv://fernandes:lucas@cluster0.kknsi5t.mongodb.net/?appName=Cluster0";
 
 let conexao = null;
 
